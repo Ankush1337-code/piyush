26443ad5d20ad7869647af86a42bffb7a4363de0
Piyush kumar <piyush1328kr@gmail.com>
2026-09-17 15:12:38 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,49 @@
+# x =10
+# y =24
+# z =7
+# print(x+y+z)
+
+# a=100;
+# b =100;
+# if a is b:
+#     print("yes")
+#     else:
+#         print("no")
+
+# def a(b,c):
+#     print(b+c)
+#     a(10,10)
+
+#how to add string
+# a ="Quad"
+# b ="AI"
+# print(a,"+",b)
+# print(a.upper())
+# print(a.lower())
+# print(a.replace("Q","k"))
+
+# a =["Quad","AI"]
+# print(a)
+# print(type(a))
+
+# c =[10,20,30,40,50]
+# print(c[0:3])
+
+d =[1,2,3,4]
+print("Before append",d)
+d.append(5)
+d.append("five")
+d.append(False)
+print("After append",d)
+
+# e=[1,2,3,4]
+# print("Before Extend",e)
+# e.extend(["five","Six",True,5])
+# print("After extend",e)
+# e.insert(3,"piyush")
+# print(e)
+
+# g=["a","b","c"]
+# print("g","=",g)
+
+
