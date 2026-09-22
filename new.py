114f67e773c877d7dd328bc1413c7874f13389c2
Piyush kumar <piyush1328kr@gmail.com>
2026-09-22 14:17:23 +0530

    Update new.py

--- new.py
+++ new.py
@@ -29,12 +29,12 @@
 # c =[10,20,30,40,50]
 # print(c[0:3])
 
-d =[1,2,3,4]
-print("Before append",d)
-d.append(5)
-d.append("five")
-d.append(False)
-print("After append",d)
+# d =[1,2,3,4]
+# print("Before append",d)
+# d.append(5)
+# d.append("five")
+# d.append(False)
+# print("After append",d)
 
 # e=[1,2,3,4]
 # print("Before Extend",e)
@@ -47,3 +47,68 @@ print("After append",d)
 # print("g","=",g)
 
 
+# x=[3,4,33,3,45,1]
+# print(x)
+# y=list((10,2,34,44,2))
+# print(type(y))
+# print(x[::-1])
+# print(y[2])
+# print(x[2])
+
+
+#insert()
+
+# x=[22,3,44,2,222,3]
+# x.insert(1,13)
+# print(x)
+
+#remove() //it removes the first occurrence of a value.
+
+# r=[44,53,33,64,53,34,22]
+# r.remove(53)
+# print(r)
+
+#output:[44, 33, 64, 34, 22]
+
+
+#pop() //removes and reurns an elment.
+# a=[10,222,222,54,11]
+# a.remove(222)
+# print(a)
+
+# print("A">"a")
+# print(ord("A"))
+# print(chr(65))
+# print("Car">"Cat")
+# print(ord("r"))
+# print(ord('t'))
+# print("car"<"cat")
+# print('''my name is piyush kumar .
+# currntly,i studdy in Quad AI.
+# here,teacher's are very helpful and also senior.''')
+# print("piyush\nkumar")  #use backward slash(\)
+
+# #pythoon.swapcase()
+# name="Piyush"
+# print(name.swapcase())
+# #casefold()
+# print(name.casefold())
+# print("python".center(50))
+# print("python".rjust(20))
+#print("python".ljust(50))
+# print("42".zfill(67))
+# print("python".index("p"))
+# print("Banana".count('a'))
+# print("Banana".startswith("B")) #gives boolen value
+# print("Python".endswith("a"))
+# print("Python".endswith("n"))
+# print("Quad".isalpha())
+# print("Quad123".isalnum())
+# print("123".isdigit())
+# print("123".isnumeric())
+# print("12".isdecimal())
+# print(" ".isspace())
+
+# x=5
+# print(-x);
+# print(+x);
