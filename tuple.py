bfa281ee747de2993c46fc44ced7af631f109d01
Piyush kumar <piyush1328kr@gmail.com>
2026-09-22 15:00:11 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,36 @@
+# imutable datatype 
+a =(10,20,30,40)
+print(a)
+print(type(a))
+
+b=(10)
+print(type(b))
+
+
+c=(10,)
+print(type(c))
+
+t=tuple("piyush")
+print(type(t))
+
+z="maths"  #it  will treat as a list.
+print(z[0])
+
+#indexing
+p=("Python","java","c++","sql")
+print(p[0])
+print(p[-1])
+print(p[-2])
+print(p[-3])
+print(p[-4])
+print(p[0:4])
+print(p[:3])
+print(p[:])
+
+
+# num = (1,2,3,5,5,5,5,6,7)
+# print(type(num))
+# print(num.count(5))
+# print(num.index(5))
+# print(len(num))
+ 
